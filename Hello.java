9f37d7809d904d2c6542a5ea330f9f59de8c6464
Dr Chao <chaombogho@gmail.com>
2016-08-11 18:23:31 +0300

    Added some output statements
    Added some output statements

--- Hello.java
+++ Hello.java
@@ -3,5 +3,7 @@ public class Hello
   public static void main(String arg[])
   {
     System.out.println("Hello World");
+    System.out.println("Hello world again");
+    System.out.println("Am I learning anything?");
   }
 }
